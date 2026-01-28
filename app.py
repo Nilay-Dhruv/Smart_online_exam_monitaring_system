0ad90618a4ec92f1610b7b9bf9dba87da2456c2c
Nilay Dhruv <nilay.dhruv121127@marwadiuniversity.ac.in>
2026-01-28 19:06:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -171,5 +171,6 @@ def logout():
     session.clear()
     return redirect(url_for('index'))
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
